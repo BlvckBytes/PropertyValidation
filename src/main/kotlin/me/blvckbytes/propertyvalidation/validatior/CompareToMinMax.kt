274deb6c55e3e83d12a4f4b08b39d07c98022413
Philip Hell <blvckbytes@gmail.com>
2023-12-02 18:22:51 +0100

    min and max of CompareToMinMax are now optional

--- src/main/kotlin/me/blvckbytes/propertyvalidation/validatior/CompareToMinMax.kt
+++ src/main/kotlin/me/blvckbytes/propertyvalidation/validatior/CompareToMinMax.kt
@@ -5,14 +5,14 @@ import kotlin.reflect.KProperty
 class CompareToMinMax<T : Comparable<T>>(
   override val field: KProperty<T?>,
   override val fieldValue: T?,
-  val min: T,
-  val max: T
+  val min: T?,
+  val max: T?
 ): ApplicableValidator<T> {
 
   override fun validate(): Boolean {
     if (fieldValue == null)
       return true
 
-    return fieldValue >= min && fieldValue <= max
+    return (min == null || fieldValue >= min) && (max == null || fieldValue <= max)
   }
 }
